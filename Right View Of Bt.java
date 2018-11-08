fee2ad25941cce4088846f4deef65ac012832805
SushilMall171096 <sushilmall96@gmail.com>
2018-11-08 23:32:23 +0530

    Right View Of Binary Tree

---
+++ Right View Of Bt.java
@@ -0,0 +1,67 @@
+import java.util.Queue;
+import java.util.LinkedList;
+class Node
+{
+	int data;
+	Node left,right;
+	Node(int data)
+	{
+	this.data=data;
+	left=right=null;
+	}
+}
+class RightView_Of_Bt
+{
+	Node root;
+	void getView(Node root)
+	{
+		if(root==null)
+			return;
+		Queue<Node> q1=new LinkedList<Node>();
+		Queue<Node> q2=new LinkedList<Node>();
+		q1.add(root);
+		while(!q1.isEmpty()||!q2.isEmpty())
+		{
+			
+			while(!q1.isEmpty())
+			{
+				Node u=q1.poll();
+				if(u.left!=null)
+					q2.add(u.left);
+				if(u.right!=null)
+					q2.add(u.right);
+				if(q1.isEmpty())
+					System.out.print(u.data+" ");
+			}
+			
+			while(!q2.isEmpty())
+			{
+				Node u=q2.poll();
+				if(u.left!=null)
+					q1.add(u.left);
+				if(u.right!=null)
+					q1.add(u.right);
+				if(q2.isEmpty())
+					System.out.print(u.data+" ");
+
+			}
+		
+		}
+
+
+	}
+	public static void main(String[] args) {
+		
+		RightView_Of_Bt tree=new RightView_Of_Bt();
+		tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7); 
+        tree.root.right.left.right = new Node(8); 
+		tree.getView(tree.root);
+	}
+
+}
